8bba6a4eb36c318c9140953fd5d550c7abe45935
JoeWex <144210911+JoeWex@users.noreply.github.com>
2023-10-20 17:46:44 +0200

    mise a jour progammation

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-// Fonction pour ouvrir le menu
+// Fonction pour ouvrir le menu burger
 function opennav() {
   document.getElementById("mySidenav").style.right = "0";
 }
-// Fonction pour fermer le menu
+// Fonction pour fermer le menu burger
 function closenav() {
   document.getElementById("mySidenav").style.right = "-250px";
 }
-// Ajoutez des gestionnaires d'événements aux boutons d'ouverture et de fermeture
+// Ajoutez des gestionnaires d'événements aux boutons d'ouverture et de fermeture du buger
 document.getElementById("openBtn").addEventListener("click", opennav);
 document.getElementById("closeBtn").addEventListener("click", closenav);
+
+
+// IMAGE ARTISTES
+// IMAGE ARTISTES
+const images = document.querySelectorAll('.artiste-image img');
+const texts = document.querySelectorAll('.artiste-text');
+
+images.forEach((img, index) => {
+  img.addEventListener('click', () => {
+    if (window.innerWidth < 801) {
+      texts[index].classList.toggle('show');
+    }
+  });
+});
